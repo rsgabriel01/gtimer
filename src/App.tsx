21dca90f53d4400947f548bd2e7b9d844b4773b3
gabriel.souza <gabriel.souza@aliare.co>
2024-02-27 13:06:12 -0300

    add theme's config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <h1>G Timer</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,6 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  primary: '#8257e6',
+  secondary: 'orange'
+}
