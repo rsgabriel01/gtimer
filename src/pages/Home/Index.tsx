f1f66a3f2b4d445007c5cea9f9d598cf7eb8ab95
Gabriel Rodrigues <rsgabriel01@gmail.com>
2024-03-14 23:07:33 -0300

    home page styles fineshed

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -3,7 +3,10 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
-  Separator
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput
 } from './styles'
 
 export function Home() {
@@ -12,10 +15,27 @@ export function Home() {
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput
+            id="task"
+            list="task-suggestion"
+            placeholder="Dê um nome para o seu projeto"
+          />
+          <datalist id="task-suggestion">
+            <option value="sdad" />
+            <option value="dasdas" />
+            <option value="sdafdsfsadfd" />
+            <option value="sdghg" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>
@@ -28,10 +48,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton type="submit">
           <PiPlay size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
