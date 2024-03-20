5a405d1146949e33b05cbf9480eca784e2128454
gabriel.souza <gabriel.souza@aliare.co>
2024-03-20 13:14:58 -0300

    Fineshed second version of history page styles

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,9 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
     <HistoryContainer>
-      <h1>Meu hisotórico</h1>
+      <h1>Meu histórico</h1>
 
       <HistoryList>
         <table>
@@ -21,35 +21,45 @@ export function History() {
               <td>Tarefa 1</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefa 1</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefa 1</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefa 1</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefa 1</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
