e80d5c12a97e4f9dd3cba4a1b615b849cb247ede
Gabriel Rodrigues <rsgabriel01@gmail.com>
2024-11-03 13:48:24 -0500

    add init time format date at history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,4 @@
+import { formatDistanceToNow } from 'date-fns'
 import { useContext } from 'react'
 import { CyclesContext } from '../../contexts/CyclesContext'
 import { HistoryContainer, HistoryList, Status } from './styles'
@@ -25,7 +26,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minuto(s)</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true
+                      // locale: ptBR
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
